f13d3d2e8d754fa0306a215686ad9144715d6820
Anders Nylund <anders.nylund@aalto.fi>
2018-05-31 16:50:36 +0300

    Solve 1.6

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,8 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            hyva: 0,
+            neutraali: 0,
+            huono: 0
+        }
+    }
+
+    render() {
+
+        return (
+            <div>
+                <h1>Anna palautetta</h1>
+                <button onClick={() => this.setState({ hyva: this.state.hyva + 1 })}>Hyvä</button>
+                <button onClick={() => this.setState({ neutraali: this.state.neutraali + 1 })}>Neutraali</button>
+                <button onClick={() => this.setState({ huono: this.state.huono + 1 })}>Huono</button>
+                <h2>Statistiikka</h2>
+                <div>Hyvä {this.state.hyva}</div>
+                <div>Neutraali {this.state.neutraali}</div>
+                <div>Huono {this.state.huono}</div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
