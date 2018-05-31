11a5839764b4ded9f78426e4610d87e4b63bb899
Anders Nylund <anders.nylund@aalto.fi>
2018-05-31 16:43:07 +0300

    Solve 1.1

--- osa1/reactin-alkeet/src/index.js
+++ osa1/reactin-alkeet/src/index.js
@@ -1,8 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const Otsikko = (props) => {
+    return (
+        <h1>{props.kurssi}</h1>
+    );
+};
+
+const Sisalto = (props) => {
+    return (
+        props.sisalto
+    );
+};
+
+const Yhteensa = (props) => {
+    return (
+        <p>Yhteensä {props.tehtavia}</p>
+    );
+};
+
+const App = () => {
+    const kurssi = 'Half Stack -sovelluskehitys'
+    const osa1 = 'Reactin perusteet'
+    const tehtavia1 = 10
+    const osa2 = 'Tiedonvälitys propseilla'
+    const tehtavia2 = 7
+    const osa3 = 'Komponenttien tila'
+    const tehtavia3 = 14
+
+    const sisalto =
+        <div>
+            <p>{osa1} {tehtavia1}</p>
+            <p>{osa2} {tehtavia2}</p>
+            <p>{osa3} {tehtavia3}</p>
+        </div>
+
+    return (
+        <div>
+            <Otsikko kurssi={kurssi}></Otsikko>
+            <Sisalto sisalto={sisalto}></Sisalto>
+            <Yhteensa tehtavia={tehtavia1 + tehtavia2 + tehtavia3}></Yhteensa>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
