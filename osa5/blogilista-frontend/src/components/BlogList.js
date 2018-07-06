948c1405947d006f37750f61eff4c46a58982ec3
Anders Nylund <anders.nylund@aalto.fi>
2018-07-06 21:03:00 +0300

    Solve 5.8

--- osa5/blogilista-frontend/src/components/BlogList.js
+++ osa5/blogilista-frontend/src/components/BlogList.js
@@ -2,9 +2,14 @@ import React from 'react';
 import Blog from './Blog';
 
 const BlogList = ({ blogs, onLike }) => {
+	
+	const sorted = blogs.sort((a, b) => {
+		return a.likes < b.likes;
+	});
+	
 	return (
 		<div>
-			{blogs.map(blog => <Blog key={blog.id} blog={blog} onLike={onLike}/>)}
+			{sorted.map(blog => <Blog key={blog.id} blog={blog} onLike={onLike}/>)}
 		</div>
 	);
 };
