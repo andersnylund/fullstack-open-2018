ab04d50835597875385d9956c9f86d40efd0bead
Anders Nylund <anders.nylund@aalto.fi>
2018-07-08 10:05:22 +0300

    Solve 5.13

--- osa5/blogilista-frontend/src/components/SimpleBlog.test.js
+++ osa5/blogilista-frontend/src/components/SimpleBlog.test.js
@@ -22,4 +22,22 @@ describe.only('<SimpleBlog />', () => {
 		expect(likeDiv.text()).toContain(blog.likes);
 
 	});
+
+	it('calls the handler when like is clicked', () => {
+		const blog = {
+			title: 'Title',
+			author: 'Author',
+			likes: 2
+		};
+
+		const mockHandler = jest.fn();
+		const blogComponent = shallow(<SimpleBlog blog={blog} onClick={mockHandler}/>);
+		// console.debug(blogComponent.debug());
+
+		const button = blogComponent.find('button');
+		button.simulate('click');
+		button.simulate('click');
+
+		expect(mockHandler.mock.calls.length).toEqual(2);
+	});
 });
